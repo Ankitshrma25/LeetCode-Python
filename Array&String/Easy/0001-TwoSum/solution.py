297a692f191c481aa218ed81b355eb49cd0b9691
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2025-10-09 22:24:54 +0530

    Frame solution

--- Array&String/Easy/0001-TwoSum/solution.py
+++ Array&String/Easy/0001-TwoSum/solution.py
@@ -1,1 +1,13 @@
-print("hello")
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        # checking for the index first of the array and then the second one 
+        for i in range(n):
+            for j in range(i+1, n):
+                # checking the sum of both indexes to be equal to the target or not
+                if nums[i] +nums[j] == target:
+                    return [i, j]
+        # Returning an empty list if no such pair is found.        
+        return []
+            
